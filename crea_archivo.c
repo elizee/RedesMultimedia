3ebd3967c631d06bea74aa55acad1ba87e6cbad1
israel navarrete <navarrete.alvarado@gmail.com>
2012-08-23 13:28:37 -0500

    correjido ejemplo escribir archivo binario

--- crea_archivo.c
+++ crea_archivo.c
@@ -1,21 +1,12 @@
-//http://es.wikibooks.org/wiki/Programaci%C3%B3n_en_C/Manejo_de_archivos
-
+/* fwrite example : write buffer */
 #include <stdio.h>
- 
-int main(int argc, char** argv)
+
+int main ()
 {
-        FILE *fp;
-        fp = fopen ( "fichero.txt", "w+" );
-        
-         char cadena[] = "Mostrando el uso de fputs en un fichero.\n";
-        char buffer[100];
-         fputs( cadena, fp );
-        
- 
-  fp = fopen ( "fichero.txt", "r+" );
-  fscanf(fp, "%s" ,buffer);
-        printf("%s",buffer);
-        
-        fclose ( fp );
-        return 0;
+  FILE * pFile;
+  char buffer[] = { 'x' , 'y' , 'z' };
+  pFile = fopen ( "myfile.bin" , "wb" );
+  fwrite (buffer , 1 , sizeof(buffer) , pFile );
+  fclose (pFile);
+  return 0;
 }
